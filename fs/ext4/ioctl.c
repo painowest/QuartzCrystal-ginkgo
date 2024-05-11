6075facaf048785adea23b449a0bf13f2de21ad1
SquareTea <164426076+SquareTea@users.noreply.github.com>
2024-05-11 22:54:24 +0700

    fs/ext4/ioctl.c : using struct ret instead of int

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -520,7 +520,7 @@ static int ext4_shutdown(struct super_block *sb, unsigned long arg)
 {
 	struct ext4_sb_info *sbi = EXT4_SB(sb);
 	__u32 flags;
-	int ret;
+	struct ret;
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
