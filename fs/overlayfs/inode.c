1224f43ce55ae336c2eae4bffa4acbc3340e64cd
SquareTea <tealovers4321@gmail.com>
2024-12-27 20:14:45 +0700

    overlayfs/inode.c:ksus fixing wrong argumentation

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -73,7 +73,7 @@ int ovl_getattr(const struct path *path, struct kstat *stat,
 	ovl_path_lowerdata(dentry, &realpath);
 	if (likely(realpath.mnt && realpath.dentry)) {
 		old_cred = ovl_override_creds(dentry->d_sb);
-		err = vfs_getattr(&realpath, stat);
+		err = vfs_getattr(&realpath, stat, request_mask, flags);
 		ovl_revert_creds(old_cred);
 		return err;
 	}
